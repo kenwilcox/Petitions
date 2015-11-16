8bba65a347e31eed16349687f601877a2e3c45e2
Ken Wilcox <wilcoxk@gmail.com>
2015-11-15 21:10:54 -0700

    Added Top Rated (10K signatures)

--- Petitions/MasterViewController.swift
+++ Petitions/MasterViewController.swift
@@ -16,7 +16,13 @@ class MasterViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+    let urlString: String
+    
+    if navigationController?.tabBarItem.tag == 0 {
+      urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+    } else {
+      urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+    }
     
     if let url = NSURL(string: urlString) {
       if let data = try? NSData(contentsOfURL: url, options: []) {
